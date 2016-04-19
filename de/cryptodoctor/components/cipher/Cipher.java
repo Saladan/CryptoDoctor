1d66642eec8578d5d281fd87bb4869029ec5e081
Saladan <MisterDLP1@gmail.com>
2016-04-19 11:34:41 +0200

    Added License Headers

--- de/cryptodoctor/components/cipher/Cipher.java
+++ de/cryptodoctor/components/cipher/Cipher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 david.
+ * Copyright 2016 David Ehnert.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
